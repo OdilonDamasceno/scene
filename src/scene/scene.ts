09325d5b3593cd92109d799231cc41c3bd6f0c4a
Matheus Henrique <matheus.henriquefont@gmail.com>
2022-10-25 21:13:19 -0300

    winterfell

--- src/scene/scene.ts
+++ src/scene/scene.ts
@@ -8,6 +8,7 @@ import {
   MeshToonMaterial,
   PlaneGeometry,
   Color,
+  BoxGeometry,
 } from "three"
 import { renderer, updateRenderer } from "/src/core/renderer"
 
@@ -39,22 +40,150 @@ const PARAMS = {
   color: "#5EDCAE",
 }
 
-const sphere = new Mesh(
-  new SphereGeometry(0.75, 32, 32),
+const tower1 = new Mesh(
+  new BoxGeometry(1, 2, 1),
   new MeshToonMaterial({
     color: new Color(PARAMS.color),
     wireframe: false,
   })
 )
 
-sphere.position.set(0, 2, 0)
-sphere.castShadow = true
+const tower2 = new Mesh(
+  new BoxGeometry(1, 2, 1),
+  new MeshToonMaterial({
+    color: new Color(PARAMS.color),
+    wireframe: false,
+  })
+)
 
-const sphereCtrls = gui.addFolder({
-  title: "Sphere",
-})
+const tower3 = new Mesh(
+  new BoxGeometry(1, 2, 1),
+  new MeshToonMaterial({
+    color: new Color(PARAMS.color),
+    wireframe: false,
+  })
+)
 
-sphereCtrls.addInput(sphere.position, "x", {
+
+const tower4 = new Mesh(
+  new BoxGeometry(1, 2, 1),
+  new MeshToonMaterial({
+    color: new Color(PARAMS.color),
+    wireframe: false,
+  })
+)
+
+const wall1 = new Mesh(
+  new BoxGeometry(3, 1.5, 1),
+  new MeshToonMaterial({
+    color: new Color(PARAMS.color),
+    wireframe: false,
+  })
+)
+
+const wall2= new Mesh(
+  new BoxGeometry(3, 1.5, 1),
+  new MeshToonMaterial({
+    color: new Color(PARAMS.color),
+    wireframe: false,
+  })
+)
+const wall3 = new Mesh(
+  new BoxGeometry(3, 1.5, 1),
+  new MeshToonMaterial({
+    color: new Color(PARAMS.color),
+    wireframe: false,
+  })
+)
+const wall4 = new Mesh(
+  new BoxGeometry(3, 1.5, 1),
+  new MeshToonMaterial({
+    color: new Color(PARAMS.color),
+    wireframe: false,
+  })
+)
+
+
+tower1.position.set(0, 1, 0)
+tower1.castShadow = true
+
+tower2.position.set(4, 1, 0)
+tower2.castShadow = true
+
+tower3.position.set(0, 1, 4)
+tower3.castShadow = true
+
+tower4.position.set(4, 1, 4)
+tower4.castShadow = true
+
+
+//WALL WALL WALL WALL /WALL WALL WALL WALL /WALL WALL WALL WALL /WALL WALL WALL WALL /WALL WALL WALL WALL
+
+
+wall1.position.set(2, 0.5, 0)
+wall1.castShadow = true
+
+wall2.position.set(4, 0.5, 2)
+wall2.castShadow = true
+wall2.rotateY(-Math.PI/2 )
+
+
+wall3.position.set(0, 0.5, 2)
+wall3.castShadow = true
+wall3.rotateY(-Math.PI/2 )
+
+
+wall4.position.set(4, 0.5, 4)
+wall4.castShadow = true
+
+
+
+
+showShadow(tower1)
+showShadow(tower2)
+showShadow(tower3)
+showShadow(tower4)
+showShadow(wall1)
+showShadow(wall2)
+showShadow(wall3)
+showShadow(wall4)
+
+scene.add(tower1)
+scene.add(tower2)
+scene.add(tower3)
+scene.add(tower4)
+scene.add(wall1)
+scene.add(wall2)
+scene.add(wall3)
+scene.add(wall4)
+
+
+const plane = new Mesh(
+  new PlaneGeometry(10, 10, 10, 10),
+  new MeshToonMaterial({
+    color: new Color("#444"),
+  })
+)
+
+plane.receiveShadow = true
+plane.rotation.set(-Math.PI / 2, 0, 0)
+scene.add(plane)
+
+
+
+export function updateScene() {
+  updateRenderer()
+}
+
+
+
+
+function showShadow(sphere:any)
+{
+  const sphereCtrls = gui.addFolder({
+    title: "Sphere",
+  })
+  sphereCtrls.addInput(sphere.position, "x", {
   label: "pos x",
   min: -10,
   max: 10,
@@ -76,21 +205,5 @@ sphereCtrls.addInput(PARAMS, "color").on("change", (e) => {
   sphere.material.color = new Color(e.value)
 })
 
-sphereCtrls.addInput(sphere.material, "wireframe")
-
-scene.add(sphere)
-
-const plane = new Mesh(
-  new PlaneGeometry(10, 10, 10, 10),
-  new MeshToonMaterial({
-    color: new Color("#444"),
-  })
-)
-
-plane.receiveShadow = true
-plane.rotation.set(-Math.PI / 2, 0, 0)
-scene.add(plane)
-
-export function updateScene() {
-  updateRenderer()
+  sphereCtrls.addInput(sphere.material, "wireframe")
 }
